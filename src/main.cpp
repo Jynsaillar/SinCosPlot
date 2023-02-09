7eeffaca4a50db0082bb074d62d2093521078923
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-02-09 11:18:03 +0100

    Changed the printSinCos(...) function to actually update the sin/cos values and added a reverse loop as well

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,25 @@ float cosValue = 0.0f;
 
 void printSinCos()
 {
-  float stepSize = 0.01f;
+  float stepSize = 0.1f;
   for (float i = 0.0f; i < PI; i += stepSize)
   {
+    sinValue = sin(i);
+    cosValue = cos(i);
     Serial.print(sinValue);
     Serial.print(",");
-    Serial.print(cosValue);
+    Serial.println(cosValue);
+    delay(100);
+  }
+
+  for (float j = PI; j > stepSize; j -= stepSize)
+  {
+    sinValue = sin(j);
+    cosValue = cos(j);
+    Serial.print(sinValue);
+    Serial.print(",");
+    Serial.println(cosValue);
+    delay(100);
   }
 }
 
@@ -24,4 +37,5 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   printSinCos(); // Prints a sine wave and a cosine wave to serial, which can be viewed on a plotter like the Arduino IDE plotter.
+  delay(250);
 }
